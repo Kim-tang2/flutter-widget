d06d733bdcbcfe6e44d21df339b05eff819e071f
kim-tang2 <>
2020-06-30 15:19:10 +0900

    layout alignment

--- lib/main.dart
+++ lib/main.dart
@@ -48,123 +48,132 @@ class Grade extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, //가로축 시작점 정렬
-          children: <Widget>[
-            Center(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 40.0, 0.0, 0.0),
+            child: Center(
               child: CircleAvatar(
                 //이미지를 원형으로
                 backgroundImage: AssetImage('assets/ogu.gif'),
                 radius: 60.0,
               ),
             ),
-            Divider(
-              //구분선 위젯
-              height: 60.0, //구분선의 위 아래 30픽셀씩 띄워주기
-              color: Colors.grey[900],
-              thickness: 0.5, //선 두께 조절
-              endIndent: 30.0, //끝에서 부터 구분선이 얼마나 떨어져야하는지
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0, //철자 간격 조정
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'OGU',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0, //철자 간격 조정
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'OGU AGE',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0, //철자 간격 조정
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '7',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0, //철자 간격 조정
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, //가로축 시작점 정렬
               children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
+
+                Divider(
+                  //구분선 위젯
+                  height: 60.0, //구분선의 위 아래 30픽셀씩 띄워주기
+                  color: Colors.grey[900],
+                  thickness: 0.5, //선 두께 조절
+                  endIndent: 30.0, //끝에서 부터 구분선이 얼마나 떨어져야하는지
                 ),
                 Text(
-                  'DANCE',
+                  'NAME',
                   style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
+                    color: Colors.white,
+                    letterSpacing: 2.0, //철자 간격 조정
                   ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'OGU',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0, //철자 간격 조정
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text(
+                  'OGU AGE',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0, //철자 간격 조정
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  '7',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0, //철자 간격 조정
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'DANCE',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'SING',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'GAME',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'SING',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
+          ),
+
+          Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/ogu.png'),
+              radius: 40.0,
+              backgroundColor: Colors.green[800],
             ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'GAME',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/ogu.png'),
-                radius: 40.0,
-                backgroundColor: Colors.green[800],
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
       drawer: Drawer(
         // leading을 써서 아이콘을 만들어주지 않아도 햄버거 모양의 아이콘이 생성된다
